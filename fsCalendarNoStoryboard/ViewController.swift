2dc5c5d4b9ee96941e7c77685b5f96e3989f287b
Gary Jeppesen <gary.jeppesen@gmail.com>
2021-03-11 06:27:36 -0700

    begin adding controls on main view

--- fsCalendarNoStoryboard/ViewController.swift
+++ fsCalendarNoStoryboard/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let mainView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    private let tableView: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = .yellow
+        return tv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(mainView)
+        
     }
 
 
+}
+
+extension ActivitiesController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "Test Cell"
+        return cell
+    }
+}
+
+extension ActivitiesController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
 }
 
